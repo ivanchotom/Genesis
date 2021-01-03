82947c3284cc8f8cd08c6b21ed831bb8e82fe120
ivanchotom <46633666+ivanchotom@users.noreply.github.com>
2021-01-03 20:46:15 +0000

    Dockspace (brocken)

--- GenesisEngine/src/ImGui/ImGuiLayer.cpp
+++ GenesisEngine/src/ImGui/ImGuiLayer.cpp
@@ -74,8 +74,8 @@ namespace GE {
 
 	void ImGuiLayer::OnImGuiRender()
 	{
-		static bool show = true;
-		ImGui::ShowDemoWindow(&show);
+		//static bool show = true;
+		//ImGui::ShowDemoWindow(&show);
 	}
 
 	void ImGuiLayer::Begin()
